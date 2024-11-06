61f007406bd033b7dba347c5f389b8c5e16c0414
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-06 01:11:33 -0600

    fix: se corrige función anualidad

--- code/funciones_actuariales.R
+++ code/funciones_actuariales.R
@@ -1,7 +1,7 @@
 anualidad_discreta <- function(x, n, i, j, inflacion){
   suma <- 0
   for (k in 1:(n-1)){
-    suma <- suma + (descuento(interes, inflacion, k) * tpx(x, k, i, j))
+    suma <- suma + (descuento(interes, inflacion, k) * tpxM(x, k, i, j))
   }
   return (suma)
 }
