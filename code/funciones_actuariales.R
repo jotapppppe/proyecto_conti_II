381c747bef7f4f914bd6f54f51c334cc2bf4155a
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-06 20:11:41 -0600

    feat: se añade anualidad post-pagable

--- code/funciones_actuariales.R
+++ code/funciones_actuariales.R
@@ -1,7 +1,12 @@
 anualidad_discreta <- function(x, n, i, j, inflacion, s){
   ifelse(s == "M", s <- lista_tablas_masc, s <- lista_tablas_fem)
-  suma <- sum(unlist(sapply(1:(n-1), function(k) descuento(interes, inflacion, k) * tpxM(x, k, i, j, s))))
+  suma <- 1 + sum(unlist(sapply(1:(n-1), function(k) descuento(interes, inflacion, k) * tpxM(x, k, i, j, s))))
   return (suma)
 }
 
-#seguro_discreto <- 
+anualidad_discreta_post <- function(x, n, i, j, inflacion, s){
+  ifelse(s == "M", s <- lista_tablas_masc, s <- lista_tablas_fem)
+  suma <- sum(unlist(sapply(1:(n), function(k) descuento(interes, inflacion, k) * tpxM(x, k, i, j, s))))
+  return (suma)
+}
+
