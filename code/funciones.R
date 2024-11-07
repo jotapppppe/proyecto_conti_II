f121484a43f1d2de857ed9b22751541369a0a3f5
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-06 21:47:11 -0600

    feat: se añade tpx en permanencia

--- code/funciones.R
+++ code/funciones.R
@@ -15,3 +15,13 @@ tpxM <- function(x, t, i, j, tabla){
   prob <- mat[i + 1, j + 1]
   return (prob)
 }
+
+tpxM_permanencia <- function(x, t, i, j, tabla){
+  prob <- 0
+  mat <- diag(4) 
+  for (k in 1:t){
+    mat <- mat * as.matrix(tabla[[as.integer(substr(as.character(x+k-1), 1, 1)) - 1]])
+  }
+  prob <- mat[i + 1, j + 1]
+  return (prob)
+}
