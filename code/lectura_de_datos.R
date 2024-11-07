e668701786dfa217280cf4a0771271efa556e792
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-06 20:04:55 -0600

    fix: cambio de tablas

--- code/lectura_de_datos.R
+++ code/lectura_de_datos.R
@@ -2,14 +2,18 @@ tablas_probabilidad_transiciones <- read_excel("data/tablas_probabilidad_transic
                                                range = "A1:F29")
 colnames(tablas_probabilidad_transiciones) <- c('Estado inicial', 'Edad', '0', '1', '2', '3')
 
+tablas_probabilidad_transiciones <- tablas_probabilidad_transiciones[order(tablas_probabilidad_transiciones$Edad), ]
+
+
 
 lista_tablas_masc <- split(tablas_probabilidad_transiciones[,3:6], 
-                      rep(1:4, each = 7))
+                      rep(1:7, each = 4))
 
 tablas_probabilidad_transiciones <- read_excel("data/tablas_probabilidad_transiciones.xlsx", 
                                                sheet = "Table2_female", range = "A1:F29")
 colnames(tablas_probabilidad_transiciones) <- c('Estado inicial', 'Edad', '0', '1', '2', '3')
+tablas_probabilidad_transiciones <- tablas_probabilidad_transiciones[order(tablas_probabilidad_transiciones$Edad), ]
 
 
 lista_tablas_fem <- split(tablas_probabilidad_transiciones[,3:6], 
-                           rep(1:4, each = 7))
+                           rep(1:7, each = 4))
