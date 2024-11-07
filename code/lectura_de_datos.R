8a9160f1e5966123cb272e60ccf7992676caf9ce
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-06 18:10:33 -0600

    fix: se agregan las tablas fem

--- code/lectura_de_datos.R
+++ code/lectura_de_datos.R
@@ -2,63 +2,14 @@ tablas_probabilidad_transiciones <- read_excel("data/tablas_probabilidad_transic
                                                range = "A1:F29")
 colnames(tablas_probabilidad_transiciones) <- c('Estado inicial', 'Edad', '0', '1', '2', '3')
 
-tabla_1 <- tablas_probabilidad_transiciones[1:7,2:6] %>%
-  group_by(Edad) %>%
-  do({
-    # Para cada grupo de edad, generar todas las edades intermedias
-    edad_inicial <- .$Edad[1]
-    edad_rango <- seq(edad_inicial, edad_inicial + 9)
-    datos_expandido <- .[rep(1, length(edad_rango)), ] 
-    datos_expandido$Edad <- edad_rango                 
-    datos_expandido
-  }) %>%
-  ungroup()
 
-tabla_2 <- tablas_probabilidad_transiciones[8:14,2:6] %>%
-  group_by(Edad) %>%
-  do({
-    # Para cada grupo de edad, generar todas las edades intermedias
-    edad_inicial <- .$Edad[1]
-    edad_rango <- seq(edad_inicial, edad_inicial + 9)
-    datos_expandido <- .[rep(1, length(edad_rango)), ] 
-    datos_expandido$Edad <- edad_rango                 
-    datos_expandido
-  }) %>%
-  ungroup()
-
-tabla_3 <- tablas_probabilidad_transiciones[15:21,2:6] %>%
-  group_by(Edad) %>%
-  do({
-    # Para cada grupo de edad, generar todas las edades intermedias
-    edad_inicial <- .$Edad[1]
-    edad_rango <- seq(edad_inicial, edad_inicial + 9)
-    datos_expandido <- .[rep(1, length(edad_rango)), ] # 
-    datos_expandido$Edad <- edad_rango                 
-    datos_expandido
-  }) %>%
-  ungroup()
-
-tabla_4 <- tablas_probabilidad_transiciones[22:28,2:6] %>%
-  group_by(Edad) %>%
-  do({
-    # Para cada grupo de edad, generar todas las edades intermedias
-    edad_inicial <- .$Edad[1]
-    edad_rango <- seq(edad_inicial, edad_inicial + 9)
-    datos_expandido <- .[rep(1, length(edad_rango)), ] 
-    datos_expandido$Edad <- edad_rango                 
-    datos_expandido
-  }) %>%
-  ungroup()
-
-tablas_estados <- list(tabla_1, tabla_2, tabla_3, tabla_4)
-
-
-rm(tabla_1)
-rm(tabla_2)
-rm(tabla_3)
-rm(tabla_4)
-
-
-lista_tablas <- split(tablas_probabilidad_transiciones[,3:6], 
+lista_tablas_masc <- split(tablas_probabilidad_transiciones[,3:6], 
                       rep(1:4, each = 7))
 
+tablas_probabilidad_transiciones <- read_excel("data/tablas_probabilidad_transiciones.xlsx", 
+                                               sheet = "Table2_female", range = "A1:F29")
+colnames(tablas_probabilidad_transiciones) <- c('Estado inicial', 'Edad', '0', '1', '2', '3')
+
+
+lista_tablas_fem <- split(tablas_probabilidad_transiciones[,3:6], 
+                           rep(1:4, each = 7))
