91e0fecfacb64ecced5fcb291897da1a2595c401
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-14 01:57:53 -0600

    fix: cambios en el permanencia

--- code/funciones.R
+++ code/funciones.R
@@ -20,7 +20,7 @@ tpxM_permanencia <- function(x, t, i, j, tabla){
   prob <- 0
   mat <- diag(4) 
   for (k in 1:t){
-    mat <- mat * as.matrix(tabla[[as.integer(substr(as.character(x+k-1), 1, 1)) - 1]])
+    mat <- mat * tabla[[as.integer(substr(as.character(x+k-1), 1, 1)) - 1]]
   }
   prob <- mat[i + 1, j + 1]
   return (prob)
