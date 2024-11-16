21b25b7a3d1123e5c4b2cd9d24cb25733d3dc1a3
jotapppppe <163952068+jotapppppe@users.noreply.github.com>
2024-11-15 21:15:12 -0600

    fix: nueva formula de la prima

--- code/funciones_actuariales.R
+++ code/funciones_actuariales.R
@@ -10,6 +10,12 @@ anualidad_discreta_post <- function(x, n, i, j, inflacion, s){
   return (suma)
 }
 
+anualidad_discreta_permanencia <- function(x, n, i, j, inflacion, s){
+  ifelse(s == "M", s <- lista_tablas_masc, s <- lista_tablas_fem)
+  suma <- 1 + sum(unlist(sapply(1:(n-1), function(k) descuento(interes, inflacion, k) * tpxM_permanencia(x, k, i, j, s))))
+  return (suma)
+}
+
 seguro <- function(x, n, i, j, inflacion, s){
   ifelse(s == "M", s <- lista_tablas_masc, s <- lista_tablas_fem)
   suma <- descuento(interes, inflacion, 1) * tpxM(x, 1, 0, i, s) + sum(unlist(sapply(2:(n), function(k) descuento(interes, inflacion, k) * tpxM(x + 1, k, i, j, s))))
@@ -26,11 +32,10 @@ seguro_permanencia <- function(x, n, i, j, inflacion, s){
 prima <- function(x, inflacion, s){
   n <- 80 - x
   m <- 65 - x
-  res <- (5e6 * seguro(x, n, 0, 3, inflacion, s) + 4e6*seguro(x, n, 1, 3, inflacion, s) + 3e6*seguro(x, n, 2, 3, inflacion, s) 
-          + 25e4 * anualidad_discreta(x, n, 2, 2, inflacion, s) + 4e3 * (seguro(x, n, 0, 3, inflacion, s) + seguro(x, n, 0, 3, inflacion, s) 
-          + seguro(x, n, 2, 3, inflacion, s))) / (anualidad_discreta(x, m, 0, 0, inflacion, s) 
-                                                                                            + 0.75 * anualidad_discreta(x, m, 0, 1, inflacion, s)) 
-                                                                                            + 0.25 * anualidad_discreta(x, m, 0, 2, inflacion, s) - 0.05
-                                                                                            - 0.05 * anualidad_discreta_post(x, n, 0, 0, inflacion, s)
+  res <- (30000000 * seguro(x, n, 0, 3, inflacion, s) + 20000000*seguro(x, n, 1, 3, inflacion, s) + 10000000*seguro(x, n, 2, 3, inflacion, s) 
+          + 2500000 * anualidad_discreta_permanencia(x, n, 2, 2, inflacion, s) + 400000 * (seguro(x, n, 0, 3, inflacion, s) + seguro(x, n, 1, 3, inflacion, s) 
+          + seguro(x, n, 2, 3, inflacion, s))) / (anualidad_discreta(x, m, 0, 0, inflacion, s) + 0.75 * anualidad_discreta(x, m, 0, 1, inflacion, s) 
+                                                                                               + 0.25 * anualidad_discreta(x, m, 0, 2, inflacion, s) - 0.05
+                                                                                               - 0.05 * anualidad_discreta_post(x, n, 0, 0, inflacion, s))
   return (res)
 }
